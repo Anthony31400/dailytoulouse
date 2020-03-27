f1bb59bb65bfe5306ee1652cab2bb7e2dcac2cfc
Anthony31400 <anthony.gensane@gmail.com>
2020-03-27 10:26:31 +0100

    Hotfix fonction reset des champs après envoi

--- featureJScontact.js
+++ featureJScontact.js
@@ -1,123 +1,102 @@
 function deactivateTooltips() {
+  var tooltips = document.querySelectorAll(".tooltip");
 
-    var tooltips = document.querySelectorAll('.tooltip');
+  var tooltipsLength = tooltips.length;
 
-    var tooltipsLength = tooltips.length;
-    
-    console.log(tooltips);
+  console.log(tooltips);
 
-    for (var i = 0; i < tooltipsLength; i++) {
-        tooltips[i].style.display = 'none';
-    }
-
+  for (var i = 0; i < tooltipsLength; i++) {
+    tooltips[i].style.display = "none";
+  }
 }
 
 function getTooltip(elements) {
-
-    while (elements = elements.nextSibling) {
-        if (elements.className === 'tooltip') {
-            return elements;
-        }
+  while ((elements = elements.nextSibling)) {
+    if (elements.className === "tooltip") {
+      return elements;
     }
+  }
 
-    return false;
-
+  return false;
 }
 
 var check = {};
 
-check['name'] = function(id) {
+check["name"] = function(id) {
+  var name = document.getElementById(id);
 
-    var name = document.getElementById(id);
+  var tooltipStyle = getTooltip(name).style;
 
-    var tooltipStyle = getTooltip(name).style;
-
-    if (name.value.length >= 3) {
-        name.className = 'correct';
-        tooltipStyle.display = 'none';
-        return true;
-    } else {
-        name.className = 'incorrect';
-        tooltipStyle.display = 'inline-block';
-        return false;
-    }
-
+  if (name.value.length >= 3) {
+    name.className = "correct";
+    tooltipStyle.display = "none";
+    return true;
+  } else {
+    name.className = "incorrect";
+    tooltipStyle.display = "inline-block";
+    return false;
+  }
 };
 
-check['email'] = function(id) {
+check["email"] = function(id) {
+  var email = document.getElementById(id);
 
-    var email = document.getElementById(id);
+  var tooltipStyle = getTooltip(email).style;
 
-    var tooltipStyle = getTooltip(email).style;
-
-    if (email.value.length >= 10) {
-        email.className = 'correct';
-        tooltipStyle.display = 'none';
-        return true;
-    } else {
-        email.className = 'incorrect';
-        tooltipStyle.display = 'inline-block';
-        return false;
-    }
-
+  if (email.value.length >= 10) {
+    email.className = "correct";
+    tooltipStyle.display = "none";
+    return true;
+  } else {
+    email.className = "incorrect";
+    tooltipStyle.display = "inline-block";
+    return false;
+  }
 };
 
-check['votreMessage'] = function(id) {
+check["votreMessage"] = function(id) {
+  var votreMessage = document.getElementById(id);
 
-    var votreMessage = document.getElementById(id);
+  var tooltipStyle = getTooltip(votreMessage).style;
 
-    var tooltipStyle = getTooltip(votreMessage).style;
-
-    if (votreMessage.value.length >= 10) {
-        votreMessage.className = 'correct';
-        tooltipStyle.display = 'none';
-        return true;
-    } else {
-        votreMessage.className = 'incorrect';
-        tooltipStyle.display = 'inline-block';
-        return false;
-    }
-
+  if (votreMessage.value.length >= 10) {
+    votreMessage.className = "correct";
+    tooltipStyle.display = "none";
+    return true;
+  } else {
+    votreMessage.className = "incorrect";
+    tooltipStyle.display = "inline-block";
+    return false;
+  }
 };
 
 (function() {
+  var myForm = document.getElementById("myForm"),
+    inputs = document.querySelectorAll("input[type=text]"),
+    inputsLength = inputs.length;
 
-    var myForm = document.getElementById('myForm'),
-        inputs = document.querySelectorAll('input[type=text]'),
-        inputsLength = inputs.length;
+  for (var i = 0; i < inputsLength; i++) {
+    inputs[i].addEventListener("keyup", function(e) {
+      check[e.target.id](e.target.id);
+    });
+  }
 
-    for (var i = 0; i < inputsLength; i++) {
-        inputs[i].addEventListener('keyup', function(e) {
-            check[e.target.id](e.target.id);
-        });
+  myForm.addEventListener("submit", function(e) {
+    var result = true;
+
+    for (var i in check) {
+      result = check[i](i) && result;
     }
 
-    myForm.addEventListener('submit', function(e) {
+    if (result) {
+      alert(
+        "Nous avons bien reçu votre message, nous reviendrons vers vous dès que possible."
+      );
+      document.getElementById("myForm").reset();
+    }
 
-        var result = true;
-
-        for (var i in check) {
-            result = check[i](i) && result;
-        }
-
-        if (result) {
-            alert('Nous avons bien reçu votre message, nous reviendrons vers vous dès que possible.');
-        }
-
-        e.preventDefault();
-
-    });
-
-    myForm.addEventListener('reset', function() {
-
-        for (var i = 0; i < inputsLength; i++) {
-            inputs[i].className = '';
-        }
-
-        deactivateTooltips();
-
-    });
-
+    e.preventDefault();
+  });
 })();
 
 deactivateTooltips();
